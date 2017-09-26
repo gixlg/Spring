55a1c070d804215da136b20685da61d965aec73f
gixlg <gix_lg@yahoo.com>
2017-09-26 23:04:29 +0200

    Created another Bean with Prototype scope, in order to get two differente instance of the same type of Bean.

--- src/main/java/it/gixlg/coding/spring/Application.java
+++ src/main/java/it/gixlg/coding/spring/Application.java
@@ -12,7 +12,13 @@ public class Application {
 	public static void main(String[] args) {
 
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
-		MessageWriter messageWriter = applicationContext.getBean(MessageWriter.class);
-		messageWriter.writeMessage();
+		MessageWriter messageWriter1 = applicationContext.getBean(MessageWriter.class);
+		MessageWriter messageWriter2 = applicationContext.getBean(MessageWriter.class);
+
+		compareObjectInstances(messageWriter1, messageWriter2);
+	}
+
+	public static void compareObjectInstances(Object obj1, Object obj2){
+		System.out.println(String.format("Is %sthe same Object",obj1==obj2 ? "" : "NOT "));
 	}
 }
--- src/main/java/it/gixlg/coding/spring/MessageWriter.java
+++ src/main/java/it/gixlg/coding/spring/MessageWriter.java
@@ -2,18 +2,22 @@ package it.gixlg.coding.spring;
 
 import it.gixlg.coding.messagebean.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by gixlg on 22/09/2017.
  */
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class MessageWriter {
 
     @Autowired
     @Qualifier("helloSpring") //or @Qualifier("helloSpringMessage")
-            Message message;
+    Message message;
 
 
     public void writeMessage(){
