5fde0dfe88cf83a050e86397563861749883dc08
gixlg <gix_lg@yahoo.com>
2017-09-26 22:09:35 +0200

    Added new Message implementation. Now for identify the right bean to use as dependency has been used @Primary annotation.

---
+++ src/main/java/it/gixlg/coding/spring/HelloSpringMessage.java
@@ -0,0 +1,16 @@
+package it.gixlg.coding.spring;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by gixlg on 26/09/2017.
+ */
+@Component
+@Primary
+public class HelloSpringMessage implements Message {
+    @Override
+    public void print() {
+        System.out.println("Hello Spring");
+    }
+}
